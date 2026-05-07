9377f5782bc3d82ade2084f6c9e58490387ddaaf
Amirhossein Mottie <zen43zem@rptu.de>
2026-05-07 02:09:47 +0200

    Revert "fix: implement nitro for vercel serverless deployment and remove static vercel.json"
    This reverts commit 81562c79c113f6da69355c0ec0155dc344401053.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
-import { nitro } from "nitro/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
@@ -8,7 +7,6 @@ export default defineConfig({
   plugins: [
     tsconfigPaths(),
     tanstackStart(),
-    nitro(),
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     tailwindcss() as any,
   ],
